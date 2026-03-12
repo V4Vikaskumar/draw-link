78df0d7b44f4891b78a82011ba284dcacefb07c5
V4Vikaskumar <vikaskumar77103@gmail.com>
2026-03-12 18:59:00 +0530

    Delete client directory

--- client/src/utils/getWorldCoords.js
+++
@@ -1,8 +0,0 @@
-export function getWorldCoords(clientX, clientY, canvasRef, camera) {
-  const rect = canvasRef.current.getBoundingClientRect();
-
-  return {
-    x: (clientX - rect.left - camera.x) / camera.zoom,
-    y: (clientY - rect.top - camera.y) / camera.zoom
-  };
-}
